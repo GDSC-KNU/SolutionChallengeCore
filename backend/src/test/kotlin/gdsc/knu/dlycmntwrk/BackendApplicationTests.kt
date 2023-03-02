cb2a91ba640d3f751172f4eb284d35a8ba4188c1
gidskql6671 <gidskql6671@gmail.com>
2023-03-02 09:48:16 +0900

    backend init

---
+++ backend/src/test/kotlin/gdsc/knu/dlycmntwrk/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package gdsc.knu.dlycmntwrk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
